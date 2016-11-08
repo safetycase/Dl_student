fa7a1bcc686e1b39efae2cfb0dd850abb4c26afc
safetycase <h_gao@sohu.com>
2016-11-08 12:53:07 +0800

    项目建立（ios7～）

---
+++ DL_student/DL_student/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DL_student
+//
+//  Created by Gao on 16/11/8.
+//  Copyright © 2016年 Gao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
